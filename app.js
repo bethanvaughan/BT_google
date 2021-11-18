ce31fb9e2b7bcf9884c26441f7a2496151f91f4f
bethanvaughan <bpvaughan97@gmail.com>
2021-11-18 09:55:54 +0200

    pre merge push

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+const http = require("http");
+
+const port = 3000;
+
+app.use(cors());
+
+const results = [
+  {
+    id: 1,
+    name: "900+ The Most Amazing Food Ever ideas in 2021",
+    link: "https://www.pinterest.co.uk/spendpennies/the-most-amazing-food-ever/",
+  },
+  {
+    id: 2,
+    name: "Amazing cakes Irish wedding cakes based in Dublin Ireland ",
+    link: "http://amazingcakes.ie",
+  },
+  {
+    id: 3,
+    name: "Top 10 Traditional British Foods",
+    link: 'https://www.delish.com/cooking/menus/g1478/quick-dinner-ideas/"',
+  },
+  {
+    id: 4,
+    name: "68 Amazingly Quick Dinners For Busy Weeknights - Delish",
+    link: 'https://www.delish.com/cooking/menus/g1478/quick-dinner-ideas/"',
+  },
+  {
+    id: 5,
+    name: "Fun Facts about Food | EatFirst Blog",
+    link: "https://www.eatfirst.com/en-au/c/blog/fun-facts-about-food",
+  },
+  {
+    id: 6,
+    name: "Impressive recipes for a special occasion - Delicious ",
+    link: "https://www.amazingfoodmadeeasy.com",
+  },
+  {
+    id: 7,
+    name: "Amazing Food Made Easy",
+    link: "https://www.amazingfoodmadeeasy.com",
+  },
+  {
+    id: 8,
+    name: "World's most delicious dishes you'll want to try | lovefood.com",
+    link: "https://www.lovefood.com/gallerylist/81561/the-worlds-most-delicious-dishes-youll-want-to-try",
+  },
+  {
+    id: 9,
+    name: "The hottest foodie destinations in Europe right now - CN",
+    link: "https://www.cntraveller.com/gallery/best-food-in-europe",
+  },
+  {
+    id: 10,
+    name: "World's most delicious dishes you'll want to try | lovefood.com",
+    link: "https://www.lovefood.com/gallerylist/81561/the-worlds-most-delicious-dishes-youll-want-to-try",
+  },
+];
+
+app.get("/results", (req, res) => res.send(results));
+
+app.listen(port, () =>
+  console.log(`Express departing now from http://localhost:${port}`)
+);
+
+app.post("/", (req, res) => {
+  res.status(405).send("not allowed");
+});
+
+module.exports = app;
---
+++ script.js
@@ -0,0 +1,20 @@
+const searchButton = document.querySelector("#search");
+searchButton.addEventListener("click", () => {
+  fetch("http://localhost:3000/results")
+    .then((res) => res.json())
+    .then(insertResults);
+  window.location.href = "result.html";
+});
+
+function insertResult(result) {
+  const link = document.createElement("div");
+  const space = document.createElement("p");
+  link.textContent = `${result.name} `;
+  space.textContent = `${result.link}`;
+  document.querySelector("#resultsspace").append(link);
+  document.querySelector("#resultsspace").append(space);
+}
+
+function insertResults(results) {
+  results.forEach((result) => insertResult(result));
+}
